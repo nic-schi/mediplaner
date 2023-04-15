b4493ffedfe1786a23a1b411b0e5cf3e1feee99e
Nichlas Schipper <Nichlas12323@gmail.com>
2023-04-15 03:21:30 +0200

    changed forms

--- seiten/anmelden/anmelden.php
+++ seiten/anmelden/anmelden.php
@@ -5,13 +5,13 @@
 
     <form id="anmelden-formular">
         <div class="input-group">
-            <input id="email" name="email" placeholder="E-Mail-Adresse" value="testuser@email.de" type="text" />
+            <input id="email" name="email" required placeholder="E-Mail-Adresse" value="testuser@email.de" type="text" />
             <label for="email">E-Mail-Adresse</label>
             <div class="feedback"></div>
         </div>
 
         <div class="input-group">
-            <input id="password" name="password" placeholder="Passwort" value="password" type="password" />
+            <input id="password" name="password" required placeholder="Passwort" value="password" type="password" />
             <label for="password">Passwort</label>
             <div class="feedback"></div>
         </div>
--- seiten/registrieren/registrieren.php
+++ seiten/registrieren/registrieren.php
@@ -5,25 +5,25 @@
 
     <form id="registrieren-formular">
         <div class="input-group">
-            <input id="username" name="username" value="testuser" placeholder="Benutzername" value="" type="text" />
+            <input id="username" name="username" required value="testuser" placeholder="Benutzername" value="" type="text" />
             <label for="username">Benutzername</label>
             <div class="feedback"></div>
         </div>
 
         <div class="input-group">
-            <input id="email" name="email" value="testuser@email.de" placeholder="E-Mail-Adresse" value="" type="text" />
+            <input id="email" name="email" required value="testuser@email.de" placeholder="E-Mail-Adresse" value="" type="text" />
             <label for="email">E-Mail-Adresse</label>
             <div class="feedback"></div>
         </div>
 
         <div class="input-group">
-            <input id="password" name="password" value="password" placeholder="Passwort" type="password" />
+            <input id="password" name="password" required value="password" placeholder="Passwort" type="password" />
             <label for="password">Passwort</label>
             <div class="feedback"></div>
         </div>
 
         <div class="input-group">
-            <input id="password-repeat" name="password-repeat" value="password" placeholder="Passwort wiederholen" type="password" />
+            <input id="password-repeat" name="password-repeat" required value="password" placeholder="Passwort wiederholen" type="password" />
             <label for="password-repeat">Passwort wiederholen</label>
             <div class="feedback"></div>
         </div>
